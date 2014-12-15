48b689baed504383fa484eda575d99989e22d5db
jakecleary <shout@jakecleary.net>
2014-12-15 14:17:03 +0000

    Add empty routes file

---
+++ app/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use Allsop\Route.php
+
+/**
+ * Define all your app routes in this file. Order them by specificity (high to
+ * low) so that requests wont be captured by generic routes when they should
+ * go to something more specific (e.g '/contact' should be picked up by the
+ * 'getContact' PageController method, not the 'show' method.
+ */
