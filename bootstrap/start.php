d4007120643495be00d5ade9708ebe90e54203bd
jakecleary <shout@jakecleary.net>
2014-12-15 14:49:58 +0000

    Add hteme bootstrap

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -6,6 +6,11 @@
 require_once(FULL_PATH . 'app/bootstrap/app.php');
 
 /**
+ * Bootstrap the theme.
+ */
+require_once(FULL_PATH . 'app/bootstrap/theme.php');
+
+/**
  * Check which route is being requested.
  */
 require_once(FULL_PATH . 'app/routes.php');
