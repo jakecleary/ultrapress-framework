e4dc3d10c0b833710d60d794970507a3d2b4f500
jakecleary <shout@jakecleary.net>
2014-12-15 14:06:08 +0000

    Define some constants and call the app bootstrap

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+* Set the key paths we'll be using
+* ================================
+* Here we set the theme path, as well as routes to all our core assets.
+*/
+
+define('PUBLIC_THEME_PATH', parse_url(get_template_directory_uri())['path'] . '/');
+define('FULL_PATH', $_SERVER['DOCUMENT_ROOT'] . PUBLIC_THEME_PATH);
+define('PUBLIC_DIR', PUBLIC_THEME_PATH . 'public/');
+define('STYLES_DIR', PUBLIC_DIR . 'styles/');
+define('SCRIPTS_DIR', PUBLIC_DIR . 'js/');
+define('IMAGES_DIR', PUBLIC_DIR . 'img/');
+
+/**
+* Initialize the app.
+*/
+include(FULL_PATH . 'app/bootstrap/app.php');
