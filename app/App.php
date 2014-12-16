0f78b1dfd6041d73d0321962b7736e7efe76e37c
jakecleary <shout@jakecleary.net>
2014-12-16 14:29:01 +0000

    Indentation

--- app/App.php
+++ app/App.php
@@ -17,11 +17,11 @@ class App
     private static $bindings = [];
 
     /**
-    * Bind an object to a key in a bindings array.
-    *
-    * @param array|string $key key to store under OR array of key => object pairs
-    * @param object $object The object we're storing OR null
-    */
+     * Bind an object to a key in a bindings array.
+     *
+     * @param array|string $key key to store under OR array of key => object pairs
+     * @param object $object The object we're storing OR null
+     */
     public static function bind($key, $object = null)
     {
         if(is_array($key))
@@ -38,12 +38,12 @@ class App
     }
 
     /**
-    * Grab an instance which has already been binded to this container
-    * otherwise we'll return null.
-    *
-    * @param string $key The key we're storing it under
-    * @return objet|boolean The object which is stored OR false
-    */
+     * Grab an instance which has already been binded to this container
+     * otherwise we'll return null.
+     *
+     * @param string $key The key we're storing it under
+     * @return objet|boolean The object which is stored OR false
+     */
     public static function instance($key)
     {
         if(isset(self::$bindings[$key]))
