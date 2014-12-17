b6bd6173035b7ffe023e6d760c34b58f0e724804
jakecleary <shout@jakecleary.net>
2014-12-17 12:37:51 +0000

    Re-add functions.php for theme initialization as functions.php get called earlier & in admin area

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -9,8 +9,3 @@ require_once(FULL_PATH . 'bootstrap/app.php');
  * Bootstrap the theme.
  */
 require_once(FULL_PATH . 'bootstrap/theme.php');
-
-/**
- * Check which route is being requested.
- */
-require_once(FULL_PATH . 'app/routes.php');
--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
 
 /**
-* Set the key paths we'll be using
-* ================================
-* Here we set the theme path, as well as routes to all our core assets.
-*/
-
-define('PUBLIC_THEME_PATH', parse_url(get_template_directory_uri())['path'] . '/');
-define('FULL_PATH', $_SERVER['DOCUMENT_ROOT'] . PUBLIC_THEME_PATH);
-define('PUBLIC_DIR', PUBLIC_THEME_PATH . 'public/');
-define('STYLES_DIR', PUBLIC_DIR . 'styles/');
-define('SCRIPTS_DIR', PUBLIC_DIR . 'js/');
-define('IMAGES_DIR', PUBLIC_DIR . 'img/');
-
-/**
-* Initialize the app.
-*/
-include(FULL_PATH . 'bootstrap/start.php');
+ * Check which route is being requested.
+ */
+require_once(FULL_PATH . 'app/routes.php');
